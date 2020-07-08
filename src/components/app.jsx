c47f14dbe39fbcac457da04ce200e80a129ff687
guillermo varela <guillovarela16@gmail.com>
2020-07-08 11:58:05 -0300

    Create new files

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return(
+      <Navbar />
+      <Banner />
+      <div className="wrapper">
+      <Cards />
+      </div>
+
+      )
+  }
+}
+
+export default App
