12e4642100fc793f72d8ba697278b298345257d4
guillermo varela <guillovarela16@gmail.com>
2020-07-17 15:55:26 -0300

    Finish the app

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 const Banner = () => {
   return(
-    <div className="banner" style={{backgroundColor: 'red'}}>
-      <p>Welcome to react</p>
-      <p>Please work this out</p>
+    <div className="banner" style={{backgroundImage: "linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)), url('https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/background.png')"}}>
+      <h1>Welcome to react</h1>
     </div>
     )
 }
 
 export default Banner
+
