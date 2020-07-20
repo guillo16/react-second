06e365121aa7989485c675f895f7c374f51ba1f3
guillermo varela <guillovarela16@gmail.com>
2020-07-20 19:15:01 -0300

    Create new component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import  NavBar from './navbar.jsx';
+import  Banner from './banner.jsx';
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Shopping'
+    }
+  }
+
   render() {
     return (
       <div>
         <NavBar />
         <Banner />
+        <Tittle title={this.state.title}/>
       </div>
       )
   }
