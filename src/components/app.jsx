672b133c111abba6eb690389bee5dd56d5de128b
guillermo varela <guillovarela16@gmail.com>
2020-08-04 15:20:42 -0300

    Create new flats

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
-import  NavBar from './navbar.jsx';
-import  Banner from './banner.jsx';
-
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: 'Shopping'
-    }
-  }
+
 
   render() {
     return (
       <div>
-        <NavBar />
-        <Banner />
-        <Tittle title={this.state.title}/>
+      hello world
       </div>
       )
   }
