b49be10a31f511b376c19f64a8ab4edd9cd85646
guillermo varela <guillovarela16@gmail.com>
2020-08-05 15:09:16 -0300

    Add google map

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import FlatList from './flat_list';
 import flats from '../data/flats';
+import SimpleMap from './google_map';
+
 
 class App extends Component {
   constructor(props) {
@@ -19,6 +21,7 @@ class App extends Component {
     return (
       <div>
         <FlatList flats={this.state.flats} selectFlat={this.selectFlat} selectedFlat={this.state.selectedFlat}/>
+        <SimpleMap />
       </div>
       )
   }
